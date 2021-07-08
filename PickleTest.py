82e55270c09bca7fabca645d3094b207bcdbd619
Danial Moradi <d.moradi@email.kntu.ac.ir>
2021-07-08 10:21:51 -0400

    Updated Pickle Test

--- PickleTest.py
+++ PickleTest.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 import pickle
 
-Index = np.random.randint(200)
-
 pickle_in_imgs = open("Dataset/imgs.pickle", "rb")
 X = pickle.load(pickle_in_imgs)
 
 pickle_in_lbls = open("Dataset/lables.pickle", "rb")
 Y = pickle.load(pickle_in_lbls)
+
+Index = np.random.randint(len(X))
 
 label = "Red" if Y[Index] == 0 else "Blue"
 cv2.imshow(label, X[Index])
